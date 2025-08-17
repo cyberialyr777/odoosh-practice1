adbec8cdd329a76ee5452354f5aa966a640e6992
Maria Fernanda Morales <112129254+cyberialyr777@users.noreply.github.com>
2025-08-17 13:06:35 -0600

    add book modulee

---
+++ my_library/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'My Library',
+    'version': '1.0',
+    'summary': 'A simple module to manage books.',
+    'author': 'Maria Fernanda',
+    'website': 'https://www.wwebsite.com',
+    'category': 'Uncategorized',
+    'depends': ['base'],
+    'data': [
+        'views/book_views.xml',
+        'security/ir.model.access.csv',
+        ],
+    'installable': True,
+    'application': True,
+}
